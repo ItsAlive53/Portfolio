e559c11354b727a8147239dbf6889ca5c90c1375
ItsAlive53 <toni.sojakka@hotmail.com>
2017-04-20 15:24:57 +0300

    fixes

--- scripts/top-scroll-button.js
+++ scripts/top-scroll-button.js
@@ -1,8 +1,9 @@
+const scrollAmount = 5;
+
 window.onscroll = function() {
     $('#topScrollButton').css({'display' : document.body.scrollTop > 5 || document.documentElement.scrollTop > 5 ? 'block' : 'none'});
 };
 
 function toTop() {
-    document.body.scrollTop = 0; // Chrome, Safari and Opera
-    document.documentElement.scrollTop = 0; // IE and FF
+    $("html").animate({scrollTop: 0}, 500);
 }
